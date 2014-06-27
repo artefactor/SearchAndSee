ff0d3ec73a304217d64a49b08c554801fb09f4c4
Alex <wisked.ab@yandex.by>
2014-06-27 10:27:49 +0300

    update sql file UTF-8

--- SearchAndSee/src/main/db/sql/insert_data.sql
+++ SearchAndSee/src/main/db/sql/insert_data.sql
@@ -1,14 +1,14 @@
-INSERT INTO `users` (`id_u`,`login`,`pass`) VALUES (1,'artem','67cce58545feb2aff157f09f87df5a392619eddf');
+п»їINSERT INTO `users` (`id_u`,`login`,`pass`) VALUES (1,'artem','67cce58545feb2aff157f09f87df5a392619eddf');
 INSERT INTO `users` (`id_u`,`login`,`pass`) VALUES (2,'borce','-3b9ff76158bf6d50bc36a3def1de0ac76ac2010c');
 INSERT INTO `users` (`id_u`,`login`,`pass`) VALUES (3,'alex','-5eef9b1bcb74fc198274ee81226bc0701701dce7');
 INSERT INTO `users` (`id_u`,`login`,`pass`) VALUES (4,'anton','-126a7b76d2b898cf4eba6cbc7257edcc7869f1fd');
 INSERT INTO `users` (`id_u`,`login`,`pass`) VALUES (5,'sasha','382670b1eefeffda9b53444b97fb2f8ef97db7f5');
 
-INSERT INTO `films` (`id_f`,`title`,`ryear`,`producer`,`category`,`description`) VALUES (1,'Побег из Шоушенка',1994,'Фрэнк Дарабонт','детективный фильм, драма','44');
-INSERT INTO `films` (`id_f`,`title`,`ryear`,`producer`,`category`,`description`) VALUES (2,'Крёстный отец',1972,'Фрэнсис Форд Коппола','гангстерский фильм, драма','sdывыв+');
-INSERT INTO `films` (`id_f`,`title`,`ryear`,`producer`,`category`,`description`) VALUES (3,'Крёстный отец2',1974,'Фрэнсис Форд Коппола','гангстерский фильм, драма','sd3');
-INSERT INTO `films` (`id_f`,`title`,`ryear`,`producer`,`category`,`description`) VALUES (4,'Тёмный рыцарь',2008,'Кристофер Нолан','триллер, драма','триллер, драма');
-INSERT INTO `films` (`id_f`,`title`,`ryear`,`producer`,`category`,`description`) VALUES (5,'Криминальное чтиво',1994,'Квентин Тарантино','sd',NULL);
+INSERT INTO `films` (`id_f`,`title`,`ryear`,`producer`,`category`,`description`) VALUES (1,'РџРѕР±РµРі РёР· РЁРѕСѓС€РµРЅРєР°',1994,'Р¤СЂСЌРЅРє Р”Р°СЂР°Р±РѕРЅС‚','РґРµС‚РµРєС‚РёРІРЅС‹Р№ С„РёР»СЊРј, РґСЂР°РјР°','44');
+INSERT INTO `films` (`id_f`,`title`,`ryear`,`producer`,`category`,`description`) VALUES (2,'РљСЂС‘СЃС‚РЅС‹Р№ РѕС‚РµС†',1972,'Р¤СЂСЌРЅСЃРёСЃ Р¤РѕСЂРґ РљРѕРїРїРѕР»Р°','РіР°РЅРіСЃС‚РµСЂСЃРєРёР№ С„РёР»СЊРј, РґСЂР°РјР°','sdС‹РІС‹РІ+');
+INSERT INTO `films` (`id_f`,`title`,`ryear`,`producer`,`category`,`description`) VALUES (3,'РљСЂС‘СЃС‚РЅС‹Р№ РѕС‚РµС†2',1974,'Р¤СЂСЌРЅСЃРёСЃ Р¤РѕСЂРґ РљРѕРїРїРѕР»Р°','РіР°РЅРіСЃС‚РµСЂСЃРєРёР№ С„РёР»СЊРј, РґСЂР°РјР°','sd3');
+INSERT INTO `films` (`id_f`,`title`,`ryear`,`producer`,`category`,`description`) VALUES (4,'РўС‘РјРЅС‹Р№ СЂС‹С†Р°СЂСЊ',2008,'РљСЂРёСЃС‚РѕС„РµСЂ РќРѕР»Р°РЅ','С‚СЂРёР»Р»РµСЂ, РґСЂР°РјР°','С‚СЂРёР»Р»РµСЂ, РґСЂР°РјР°');
+INSERT INTO `films` (`id_f`,`title`,`ryear`,`producer`,`category`,`description`) VALUES (5,'РљСЂРёРјРёРЅР°Р»СЊРЅРѕРµ С‡С‚РёРІРѕ',1994,'РљРІРµРЅС‚РёРЅ РўР°СЂР°РЅС‚РёРЅРѕ','sd',NULL);
 
 INSERT INTO `sitebase`.`usersrate` (`id_r`, `id_u`, `id_f`, `p_rate`) VALUES ('1', '1', '1', '1');
 INSERT INTO `sitebase`.`usersrate` (`id_r`, `id_u`, `id_f`, `p_rate`) VALUES ('2', '1', '2', '10');
